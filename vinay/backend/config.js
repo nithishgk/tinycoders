44b8b9b278246fe8f117baa230f33e252230d64b
vinaykumar <vinay.g@coMakeIT.com>
2022-04-07 17:51:10 +0530

    code

---
+++ vinay/backend/config.js
@@ -0,0 +1,10 @@
+module.exports ={
+    MONGO_URI:'mongodb+srv://nithish:nithish@clusterq.lscpb.mongodb.net/test'
+}
+
+
+//nithish
+//5oQZLimUHyQ6NaKa
+
+//mongodb+srv://nithish:5oQZLimUHyQ6NaKa@cluster0.lscpb.mongodb.net/test
+//mongodb+srv://keer:MryhrvFjTBWJl4rT@cluster0.lhoj0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
